a2bda3785f9c30260e36eb93ae57c190b01679ee
Rajesh Korde <rajesh.korde@gmail.com>
2025-03-17 13:15:54 -0700

    testing trl

--- training/supervised_finetuning/trl_quickstart.py
+++ training/supervised_finetuning/trl_quickstart.py
@@ -1,4 +1,21 @@
-# https://huggingface.co/docs/trl/quickstart
-
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from trl import SFTConfig, SFTTrainer
+
+
+class SFTConfigWithMPS(SFTConfig):
+    @property
+    def device(self) -> torch.device:
+        if torch.backends.mps.is_available():
+            return torch.device("mps")
+        else:
+            return torch.device("cpu")
+
+
+dataset = load_dataset("stanfordnlp/imdb", split="train")
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+training_args = SFTConfigWithMPS(max_seq_length=512, output_dir="/tmp")
+trainer = SFTTrainer("facebook/opt-350m", train_dataset=dataset, args=training_args)
+
+trainer.train()
