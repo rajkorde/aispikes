6422f8f9c840b86a49b9e2f363dbc7e849989608
Rajesh Korde <rajesh.korde@gmail.com>
2025-04-27 14:14:42 -0700

    added feature flags

--- spikes/storyteller/cli.py
+++ spikes/storyteller/cli.py
@@ -3,13 +3,16 @@ import uuid
 import typer
 from dotenv import load_dotenv
 from rich.prompt import IntPrompt, Prompt
+from src.feature_flags import FeatureFlags
 from src.story import Story, StoryCondition, Student
+from src.utils import deserialize, serialize
 
 assert load_dotenv()
 
 __version__ = "0.0.1"
 
 app = typer.Typer()
+flags = FeatureFlags.read_feature_flags()
 
 
 def version_callback(value: bool):
@@ -45,34 +48,45 @@ def ask_student_questions() -> dict[str, str | int]:
 
 @app.command()
 def get_student_info():
-    print_header()
-    responses = ask_student_questions()
+    if flags.get_story_situation:
+        print_header()
+        responses = ask_student_questions()
 
-    typer.secho("\nYour responses:", fg=typer.colors.CYAN, bold=True)
-    for key, value in responses.items():
-        typer.echo(f"{key.capitalize()}: {value}")
+        typer.secho("\nYour responses:", fg=typer.colors.CYAN, bold=True)
+        for key, value in responses.items():
+            typer.echo(f"{key.capitalize()}: {value}")
 
-    assert isinstance(responses["name"], str)
-    assert isinstance(responses["interests"], str)
-    assert isinstance(responses["age"], int)
+        assert isinstance(responses["name"], str)
+        assert isinstance(responses["interests"], str)
+        assert isinstance(responses["age"], int)
 
-    scenario_id = str(uuid.uuid4())
-    student = Student(
-        name=responses["name"],
-        interests=responses["interests"],
-        age=responses["age"],
-        scenario_id=scenario_id,
-    )
+        scenario_id = str(uuid.uuid4())
+        student = Student(
+            name=responses["name"],
+            interests=responses["interests"],
+            age=responses["age"],
+            scenario_id=scenario_id,
+        )
 
-    assert isinstance(responses["situation"], str)
-    assert isinstance(responses["guidance"], str | None)
-    guidance = None if not responses["guidance"] else responses["guidance"]
-    story_condition = StoryCondition(
-        scenario_id=scenario_id,
-        student=student,
-        situation=responses["situation"],
-        guidance=guidance,
-    )
+        assert isinstance(responses["situation"], str)
+        assert isinstance(responses["guidance"], str | None)
+        guidance = None if not responses["guidance"] else responses["guidance"]
+        story_condition = StoryCondition(
+            scenario_id=scenario_id,
+            student=student,
+            situation=responses["situation"],
+            guidance=guidance,
+        )
+
+        if flags.save_data:
+            serialize(story_condition, f"data/{scenario_id}/story_condition.json")
+
+    else:
+        scenario_id = "3ae4e510-9a0d-4b44-b771-6a20d3bb433d"
+        story_condition = deserialize(
+            f"data/{scenario_id}/story_condition.json",
+            StoryCondition,
+        )
 
     print(f"{story_condition=}")
 
